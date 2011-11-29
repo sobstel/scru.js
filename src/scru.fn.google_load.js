a0da28504f183dcd2f164536fbab2e35dcf44f85
Przemek Sobstel <przemek@sobstel.org>
2011-11-29 20:40:00 +0100

    Fix code formatting.

--- src/scru.fn.google_load.js
+++ src/scru.fn.google_load.js
@@ -1,13 +1,13 @@
 /* scru.js, (c) Przemek Sobstel 2011, License: MIT */
 
 $scru.fn.google_load = function(moduleName, version, optionalSettings) {
-	optionalSettings = optionalSettings || []
-	var org_callback = optionalSettings['callback'] || function(){}
-	return function(id) {
-		optionalSettings['callback'] = function() { 
-			$scru.completed(id)
-			org_callback()
-		}
-  	google.load(moduleName, version, optionalSettings)
-	}
+  optionalSettings = optionalSettings || []
+  var org_callback = optionalSettings['callback'] || function(){}
+  return function(id) {
+    optionalSettings['callback'] = function() {
+      $scru.completed(id)
+      org_callback()
+    }
+    google.load(moduleName, version, optionalSettings)
+  }
 }
